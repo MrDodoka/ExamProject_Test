3869e6f76e0c1fc9b93ba7dd399b8cc15b44aa44
Mr.Dodo ❤ 寂しい <bobence24@gmail.com>
2022-01-26 09:49:31 +0100

    Added CoinGecko API wrapper
    Added the CoinGeckoAsyncAPI nuget

---
+++ ExamProject_Test/Controllers/CoinGeckoController.cs
@@ -0,0 +1,20 @@
+﻿using CoinGecko.Clients;
+using CoinGecko.Entities.Response.Coins;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ExamProject_Test.Controllers
+{
+    public class CoinGeckoController
+    {
+        CoinGeckoClient CG = new();
+
+        public async Task<List<CoinMarkets>>TestCGMethod()
+        {
+
+            var asd = await CG.CoinsClient.GetCoinMarkets("usd", new string[] { }, null, null, null, false, null, null);
+
+            return asd;
+        }
+    }
+}
---
+++ ExamProject_Test/Pages/CoinGeckoMethodTest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExamProject_Test.Pages
+{
+    public class CoinGeckoMethodTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
